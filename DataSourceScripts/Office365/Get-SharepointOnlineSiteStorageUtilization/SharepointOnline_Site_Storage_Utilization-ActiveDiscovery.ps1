7d98a2f6147ab45b6c9ad663da75bfaea6de0913
Mike Hashemi <mhashemi@synoptek.com>
2019-09-17 15:36:47 -0600

    Moved files + changed log name.

--- DataSourceScripts/Office365/Get-SharepointOnlineSiteStorageUtilization/SharepointOnline_Site_Storage_Utilization-ActiveDiscovery.ps1
+++ DataSourceScripts/Office365/Get-SharepointOnlineSiteStorageUtilization/SharepointOnline_Site_Storage_Utilization-ActiveDiscovery.ps1
@@ -1,6 +1,4 @@
 # Initialize variables.
-$siteUrl = '##wildValue##'
-$siteName = '##wildAlias##'
 $adminSiteURL = '##sharepointonlineadmin.url##'
 $cred = New-Object System.Management.Automation.PSCredential ('##sharepointonlineadmin.user##', ('##sharepointonlineadmin.pass##' | ConvertTo-SecureString -AsPlainText -Force))
 
@@ -10,10 +8,32 @@ If (Test-Path -Path "C:\Program Files (x86)\LogicMonitor\Agent\Logs" -ErrorActio
 Else {
     $logDirPath = "$([System.Environment]::SystemDirectory)" # Directory, into which the log file will be written.
 }
-$logFile = "$logDirPath\datasource-SharepointOnline_Storage_Utilization-collection-$siteName.log"
+$logFile = "$logDirPath\datasource-SharepointOnline_Site_Storage_Utilization-ad-$(($adminSiteURL.Split('/')[-1]).Split('-')[0]).log"
 
 $message = ("{0}: Beginning {1}." -f [datetime]::Now, $MyInvocation.MyCommand)
 If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile } Else { $message | Out-File -FilePath $logFile }
+
+$message = ("{0}: Checking if the Microsoft.Online.SharePoint.PowerShell module is installed." -f [datetime]::Now)
+If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+
+If (-NOT (Get-Module -Name Microsoft.Online.SharePoint.PowerShell -ListAvailable)) {
+    $message = ("{0}: The Microsoft.Online.SharePoint.PowerShell module is not installed. Attempting to install it." -f [datetime]::Now)
+    If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+
+    Try {
+        Install-Module -Name Microsoft.Online.SharePoint.PowerShell -Force
+    }
+    Catch {
+        $message = ("{0}: Unexpected error installing the Microsoft.Online.SharePoint.PowerShell module. The specific error is: {1}" -f [datetime]::Now, $_.Exception.Message)
+        If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+
+        Exit 1
+    }
+}
+Else {
+    $message = ("{0}: The Microsoft.Online.SharePoint.PowerShell module is installed." -f [datetime]::Now)
+    If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+}
 
 $message = ("{0}: Attempting to connect to {1} with {2}." -f [datetime]::Now, $adminSiteURL, $cred.UserName)
 If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
@@ -41,27 +61,14 @@ Catch {
     Exit 1
 }
 
-$site = $siteCollections.Where( { $_.Title -eq $siteName })
+$message = ("{0}: Found {1} sites." -f [datetime]::Now, $siteCollections.count)
+If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
 
-If ($site.Status -eq 'Active') {
-    $data = [PSCustomObject]@{
-        Allocated   = $site.StorageQuota
-        Used        = $site.StorageUsageCurrent
-        PercentUsed = [math]::Round((($site.StorageUsageCurrent / $site.StorageQuota) * 100), 2)
-    }
-
-    $message = ("{0}: Found:`r`nAllocatedStorage={1}`r`nUsedStorage={2}`r`nPercentUsed={3}." -f [datetime]::Now, $data.Allocated, $data.Used, $data.PercentUsed)
+Foreach ($site in $siteCollections) {
+    $message = ("{0}: Returning {1}." -f [datetime]::Now, $site.title)
     If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
 
-    Write-Host ("Allocated={0}" -f $data.Allocated)
-    Write-Host ("Used={0}" -f $data.Used)
-    Write-Host ("PercentUsed={0}" -f $data.PercentUsed)
-
-    Exit 0
+    Write-Host "$($site.url)##$($site.title)"
 }
-Else {
-    $message = ("{0}: {1} is not active." -f [datetime]::Now, $siteName)
-    If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
 
-    Exit 0
-}
+Exit 0
