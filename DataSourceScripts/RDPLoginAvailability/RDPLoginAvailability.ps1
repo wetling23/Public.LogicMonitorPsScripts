e2340a0a3cc2614ac97acbe3b02a2e6b329b219e
Mike Hashemi <mhashemi@synoptek.com>
2019-10-09 11:45:09 -0600

    Updated script after testing.

--- DataSourceScripts/RDPLoginAvailability/RDPLoginAvailability.ps1
+++ DataSourceScripts/RDPLoginAvailability/RDPLoginAvailability.ps1
@@ -5,6 +5,7 @@
         Author: Mike Hashemi
         V1.0.0.0 date: 13 September 2019
             - Initial release
+        V1.0.0.1 date: 9 October 2019
     .LINK
         https://github.com/wetling23/Public.LogicMonitorPsScripts/tree/master/DataSourceScripts/RDPLoginAvailability
 #>
@@ -34,7 +35,6 @@ Function Connect-RDP {
 # Initialize variables.
 $computerName = '##system.hostname##'
 $username = '##rdp.user##'
-#$password = 
 $cred = New-Object System.Management.Automation.PSCredential ($username, $('##rdp.pass##' | ConvertTo-SecureString -AsPlainText -Force))
 
 If (Test-Path -Path "C:\Program Files (x86)\LogicMonitor\Agent\Logs" -ErrorAction SilentlyContinue) {
@@ -48,10 +48,21 @@ $logFile = "$logDirPath\datasource-RDP_Login_Availability-collection-$computerNa
 $message = ("{0}: Beginning {1}." -f [datetime]::Now, $MyInvocation.MyCommand)
 If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile } Else { $message | Out-File -FilePath $logFile }
 
-$message = ("{0}: Checking if the AuthenticationLevelOverride registry value is present." -f [datetime]::Now)
+$message = ("{0}: Checking if the HKCU:\Software\Microsoft\Terminal Server Client path is present." -f [datetime]::Now)
 If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
 
-$rdpSecStatus = (Get-ItemProperty -Path 'HKCU:\Software\Microsoft\Terminal Server Client\').AuthenticationLevelOverride
+If (Test-Path -Path 'HKCU:\Software\Microsoft\Terminal Server Client') {
+    $message = ("{0}: Found path, checking AuthenticationLevelOverride value." -f [datetime]::Now)
+    If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+
+    $rdpSecStatus = (Get-ItemProperty -Path 'HKCU:\Software\Microsoft\Terminal Server Client\' -ErrorAction SilentlyContinue).AuthenticationLevelOverride
+}
+Else {
+    $message = ("{0}: Path not found, creating it." -f [datetime]::Now)
+    If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+
+    New-Item -Path 'HKCU:\Software\Microsoft\Terminal Server Client\'
+}
 
 If ($rdpSecStatus -and ($rdpSecStatus -eq 0)) {
     $message = ("{0}: Found AuthenticationLevelOverride present and with value '0'. No changes to make." -f [datetime]::Now)
@@ -94,6 +105,7 @@ $response = Invoke-Command -ComputerName $computerName -Credential $cred -Script
         $ComputerName,
         $UserName
     )
+
     Function Get-ActiveSessions {
         Param(
             [Parameter(
@@ -102,24 +114,27 @@ $response = Invoke-Command -ComputerName $computerName -Credential $cred -Script
                 ValueFromPipelineByPropertyName = $true
             )]
             [ValidateNotNullOrEmpty()]
-            [string]$Name
-            ,
+            [string]$ComputerName,
+
             [switch]$Quiet
         )
         Begin {
             $return = @()
         }
         Process {
-            If (!(Test-Connection $Name -Quiet -Count 1)) {
-                Write-Error -Message "Unable to contact $Name. Please verify its network connectivity and try again." -Category ObjectNotFound -TargetObject $Name
-                Return
+            If (-NOT(Test-Connection $ComputerName -Quiet -Count 1)) {
+                $Script:message += ("{0}: Unable to contact $ComputerName. Please verify its network connectivity and try again.`n" -f [datetime]::Now)
+
+                Return "Error"
             }
             If ([bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) {
                 #check if user is admin, otherwise no registry work can be done
+                $Script:message += ("{0}: Verified that we are running as an admin.`n" -f [datetime]::Now)
+
                 #the following registry key is necessary to avoid the error 5 access is denied error
                 $LMtype = [Microsoft.Win32.RegistryHive]::LocalMachine
                 $LMkey = "SYSTEM\CurrentControlSet\Control\Terminal Server"
-                $LMRegKey = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($LMtype, $Name)
+                $LMRegKey = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($LMtype, $ComputerName)
                 $regKey = $LMRegKey.OpenSubKey($LMkey, $true)
                 If ($regKey.GetValue("AllowRemoteRPC") -ne 1) {
                     $regKey.SetValue("AllowRemoteRPC", 1)
@@ -128,17 +143,27 @@ $response = Invoke-Command -ComputerName $computerName -Credential $cred -Script
                 $regKey.Dispose()
                 $LMRegKey.Dispose()
             }
-            $result = qwinsta /server:$Name
+            Else {
+                $Script:message += ("{0}: Verified that we are not running as an admin.`n" -f [datetime]::Now)
+            }
+
+            $Script:message += ("{0}: Running qwinsta against {1}.`n" -f [datetime]::Now, $ComputerName)
+
+            $result = qwinsta /server:$ComputerName
+
             If ($result) {
+                $Script:message += ("{0}: Found sessions.`n" -f [datetime]::Now)
+
                 ForEach ($line in $result[1..$result.count]) {
                     #avoiding the line 0, don't want the headers
                     $tmp = $line.split(" ") | ? { $_.length -gt 0 }
                     If (($line[19] -ne " ")) {
+
                         #username starts at char 19
                         If ($line[48] -eq "A") {
                             #means the session is active ("A" for active)
                             $return += New-Object PSObject -Property @{
-                                "ComputerName" = $Name
+                                "ComputerName" = $ComputerName
                                 "SessionName"  = $tmp[0]
                                 "UserName"     = $tmp[1]
                                 "ID"           = $tmp[2]
@@ -148,7 +173,7 @@ $response = Invoke-Command -ComputerName $computerName -Credential $cred -Script
                         }
                         Else {
                             $return += New-Object PSObject -Property @{
-                                "ComputerName" = $Name
+                                "ComputerName" = $ComputerName
                                 "SessionName"  = $null
                                 "UserName"     = $tmp[0]
                                 "ID"           = $tmp[1]
@@ -158,9 +183,11 @@ $response = Invoke-Command -ComputerName $computerName -Credential $cred -Script
                         }
                     }
                 }
+
+                $result
             }
             Else {
-                Write-Error "Unknown error, cannot retrieve logged on users"
+                $Script:message += ("{0}: Unknown error, cannot retrieve logged on users.`n" -f [datetime]::Now)
             }
         }
         End {
@@ -174,7 +201,7 @@ $response = Invoke-Command -ComputerName $computerName -Credential $cred -Script
             }
             Else {
                 If (!($Quiet)) {
-                    Write-Host "No active sessions."
+                    $Script:message += "{0}: No active sessions.`n" -f [datetime]::Now
                 }
                 Return $false
             }
@@ -186,7 +213,7 @@ $response = Invoke-Command -ComputerName $computerName -Credential $cred -Script
                 Closes the specified open sessions of a remote or local workstations
             .DESCRIPTION
                 Close-ActiveSessions uses the command line tool rwinsta to close the specified open sessions on a computer whether they are RDP or logged on locally.
-            .PARAMETER $Name
+            .PARAMETER $ComputerName
                 The name of the computer that you would like to close the active sessions on.
             .PARAMETER ID
                 The ID number of the session to be closed.
@@ -199,7 +226,7 @@ $response = Invoke-Command -ComputerName $computerName -Credential $cred -Script
 
                 Closes all sessions that are not active on DC01
             .INPUTS
-                [string]$Name
+                [string]$ComputerName
                 [int]ID
             .OUTPUTS
                 Progress of the rwinsta command.
@@ -218,6 +245,7 @@ $response = Invoke-Command -ComputerName $computerName -Credential $cred -Script
                 Position = 1
             )]
             [string]$ComputerName,
+
             [Parameter(
                 Mandatory = $true,
                 ValueFromPipeline = $true,
@@ -228,16 +256,17 @@ $response = Invoke-Command -ComputerName $computerName -Credential $cred -Script
         )
         Begin { }
         Process {
-            If (!(Test-Connection $ComputerName -Quiet -Count 1)) {
-                Write-Error -Message "Unable to contact $ComputerName. Please verify its network connectivity and try again." -Category ObjectNotFound -TargetObject $ComputerName
-                Return
+            If (-NOT(Test-Connection $ComputerName -Quiet -Count 1)) {
+                $Script:message += ("{0}: Unable to contact {1}. Please verify its network connectivity and try again.`n" -f [datetime]::Now, $ComputerName)
+
+                Return "Error"
             }
             If ([bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) {
                 #check if user is admin, otherwise no registry work can be done
                 #the following registry key is necessary to avoid the error 5 access is denied error
                 $LMtype = [Microsoft.Win32.RegistryHive]::LocalMachine
                 $LMkey = "SYSTEM\CurrentControlSet\Control\Terminal Server"
-                $LMRegKey = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($LMtype, $Name)
+                $LMRegKey = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($LMtype, $ComputerName)
                 $regKey = $LMRegKey.OpenSubKey($LMkey, $true)
                 If ($regKey.GetValue("AllowRemoteRPC") -ne 1) {
                     $regKey.SetValue("AllowRemoteRPC", 1)
@@ -246,26 +275,51 @@ $response = Invoke-Command -ComputerName $computerName -Credential $cred -Script
                 $regKey.Dispose()
                 $LMRegKey.Dispose()
             }
-            rwinsta.exe /server:$ComputerName $ID /V
+
+            $Script:message += ("{0}: Running rwinsta to log off the user with session ID {1}.`n" -f [datetime]::Now, $ID)
+
+            $script:message += rwinsta.exe /server:$ComputerName $ID /V
         }
         End { }
     }
 
-    $message = ("{0}: Getting active RDP sessions on {1}." -f [datetime]::Now, $ComputerName)
-    If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+    $message = @"
+{0}: Getting active RDP sessions on {1}. We will look for {2}.`n
+"@ -f [datetime]::Now, $ComputerName, $UserName
+    Set-Variable -Name message -Option AllScope
 
-    $sessions = Get-ActiveSessions -Name $ComputerName
+    $sessions = Get-ActiveSessions -ComputerName $ComputerName
 
-    $message = ("{0}: Checking if {1} is logged in." -f [datetime]::Now, $UserName)
-    If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+    If ($sessions.UserName) {
+        $message += ("{0}: Found {1} sessions. Checking if {2} is logged in.`n" -f [datetime]::Now, $sessions.Count, $UserName)
 
-    If ($sessions.UserName -match $UserName) {
-        $message = ("{0}: Found {1} logged in. The session ID is {2}." -f [datetime]::Now, $UserName, ($sessions | Where-Object { $_.UserName -match $UserName }).Id)
-        If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+        $sessions | ForEach-Object {
+            If ($_.UserName -eq $UserName.Split('\')[-1]) {
+                $message += ("{0}: Found {1} logged in. The session ID is {2}. Calling Close-ActiveSessions.`n" -f [datetime]::Now, $UserName, $_.Id)
 
-        Close-ActiveSessions -ComputerName $ComputerName -Id ($sessions | Where-Object { $_.UserName -match $UserName }).Id
+                $response = Close-ActiveSessions -ComputerName $ComputerName -Id $_.Id
+
+                If ($response -eq "Error") {
+                    Return 2, $message
+                }
+                Else {
+                    Return 0, $message
+                }
+            }
+        }
     }
-} -ArgumentList $computerName, $username
+    ElseIf ($sessions -eq "Error") {
+        Return 1, $message
+    }
+    Else {
+        $message += ("{0}: No sessions matching {1} found. Disconnecting from {2}." -f [datetime]::Now, $UserName, $ComputerName)
+
+        Return 3, $message
+    }
+} -ArgumentList $computerName, $username -Verbose
+
+# Adding response to the DataSource log.
+If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $response[-1]; $response[-1] | Out-File -FilePath $logFile -Append } Else { $response[-1] | Out-File -FilePath $logFile -Append }
 
 $message = ("{0}: If necessary, returning the registry to its previous state." -f [datetime]::Now)
 If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
@@ -306,7 +360,7 @@ If ($response -match 'Resetting session ID') {
     Exit 0
 }
 Else {
-    $message = ("{0}: Found login to {1} unsuccessful." -f [datetime]::Now, $computerName)
+    $message = ("{0}: Test login to {1} unsuccessful." -f [datetime]::Now, $computerName)
     If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
 
     Exit 1
