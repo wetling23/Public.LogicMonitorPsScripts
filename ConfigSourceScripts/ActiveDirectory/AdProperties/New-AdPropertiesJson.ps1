8b3a64982a7a47ae62ea1d6960d2d413c77c1f35
Mike Hashemi <mhashemi@synoptek.com>
2019-11-06 12:25:26 -0700

    Fiddling /w json in ps v2.

--- ConfigSourceScripts/ActiveDirectory/AdProperties/New-AdPropertiesJson.ps1
+++ ConfigSourceScripts/ActiveDirectory/AdProperties/New-AdPropertiesJson.ps1
@@ -742,17 +742,78 @@ Function Get-GpoSettings {
 
     $script:backup | Add-Member -MemberType NoteProperty -Name GPOs -Value @($gpoCsv) -Force
 }
-Function ConvertTo-Json20 {
-    [CmdletBinding()]
-    param (
-        [object]$Item
-    )
+Function Escape-JSONString ($str) {
+    #https://gist.github.com/mdnmdn/6936714
+    if ($str -eq $null) { return "" }
+    $str = $str.ToString().Replace('"', '\"').Replace('\', '\\').Replace("`n", '\n').Replace("`r", '\r').Replace("`t", '\t')
+    return $str;
+}
+Function ConvertTo-Json20 ($maxDepth = 4, $forceArray = $false) {
+    #https://gist.github.com/mdnmdn/6936714
+    begin {
+        $data = @()
+    }
+    process {
+        $data += $_
+    }
+    end {
+        if ($data.length -eq 1 -and $forceArray -eq $false) {
+            $value = $data[0]
+        }
+        else {
+            $value = $data
+        }
 
-    Add-Type -Assembly system.web.extensions
-    
-    $ps_js = New-Object system.web.script.serialization.javascriptSerializer
+        if ($value -eq $null) {
+            return "null"
+        }
 
-    Return $ps_js.Serialize($item)
+        $dataType = $value.GetType().Name
+
+        switch -regex ($dataType) {
+            'String' {
+                return "`"{0}`"" -f (Escape-JSONString $value )
+            }
+            '(System\.)?DateTime' { return "`"{0:yyyy-MM-dd}T{0:HH:mm:ss}`"" -f $value }
+            'Int32|Double' { return "$value" }
+            'Boolean' { return "$value".ToLower() }
+            '(System\.)?Object\[\]' {
+                # array
+                if ($maxDepth -le 0) { return "`"$value`"" }
+
+                $jsonResult = ''
+                foreach ($elem in $value) {
+                    #if ($elem -eq $null) {continue}
+                    if ($jsonResult.Length -gt 0) { $jsonResult += ', ' }
+                    $jsonResult += ($elem | ConvertTo-Json20 -maxDepth ($maxDepth - 1))
+                }
+                return "[" + $jsonResult + "]"
+            }
+            '(System\.)?Hashtable' {
+                # hashtable
+                $jsonResult = ''
+                foreach ($key in $value.Keys) {
+                    if ($jsonResult.Length -gt 0) { $jsonResult += ', ' }
+                    $jsonResult += 
+                    @"
+"{0}": {1}
+"@ -f $key , ($value[$key] | ConvertTo-Json20 -maxDepth ($maxDepth - 1) )
+                }
+                return "{" + $jsonResult + "}"
+            }
+            default {
+                #object
+                if ($maxDepth -le 0) { return  "`"{0}`"" -f (Escape-JSONString $value) }
+
+                return "{" +
+                (($value | Get-Member -MemberType *property | ForEach-Object {
+                            @"
+"{0}": {1}
+"@ -f $_.Name , ($value.($_.Name) | ConvertTo-Json20 -maxDepth ($maxDepth - 1) )
+                        }) -join ', ') + "}"
+            }
+        }
+    }
 }
 # This section puts the Get-GpoReportData function definition into $code, so I can send the function to the remote DC.
 $code = @"
@@ -918,8 +979,8 @@ If ($backup.ForestName) {
     If ($PSBoundParameters['Verbose']) {Write-Verbose $message; $message | Out-File -FilePath $logFile -Append} Else {$message | Out-File -FilePath $logFile -Append}
 
     If ($PSVersionTable.PSVersion -eq '2.0') {
-        ConvertTo-Json20 -Item $backup | Out-File -FilePath "$([System.Environment]::SystemDirectory)\adConfig.json" -Force
-        ConvertTo-Json20 -Item $backup
+        $backup | ConvertTo-Json20 | Out-File -FilePath "$([System.Environment]::SystemDirectory)\adConfig.json" -Force
+        $backup | ConvertTo-Json20
     }
     Else {
         $backup | ConvertTo-Json | Out-File -FilePath "$([System.Environment]::SystemDirectory)\adConfig.json" -Force
