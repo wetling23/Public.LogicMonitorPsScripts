9ae7d3dbd000a1fdb8c82e8601d24e2337d00cc0
Mike Hashemi <mhashemi@synoptek.com>
2021-09-13 10:34:11 -0600

    Added Chrome options and logging

--- DataSourceScripts/Selenium/SuccessWareASP/Test-SuccessWareAspLogin.ps1
+++ DataSourceScripts/Selenium/SuccessWareASP/Test-SuccessWareAspLogin.ps1
@@ -6,6 +6,7 @@
         V1.0.0.0 date: 1 October 2020
         V1.0.0.1 date: 29 October 2020
         V1.0.0.2 date: 22 January 2021
+        V1.0.0.3 date: 13 September 2021
     .LINK
         https://github.com/wetling23/Public.LogicMonitorPsScripts/tree/master/DataSourceScripts/Selenium/SuccessWareASP
 #>
@@ -153,18 +154,21 @@ If ($result -eq 1) {
     Exit 1
 }
 
-$message = ("{0}: Starting chromedriver.exe." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+$message = ("{0}: Starting Chrome." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
 Write-Host $message; $message | Out-File -FilePath $logFile -Append
 
 Try {
     # Invoke Selenium into our script!
     $env:PATH += ";$seleniumPath" # Adds the path for ChromeDriver.exe to the environmental variable 
     Add-Type -Path "$seleniumPath\WebDriver.dll" # Adding Selenium's .NET assembly (dll) to access it's classes in this PowerShell session
-    $chromedriver = New-Object OpenQA.Selenium.Chrome.ChromeDriver # Creates an instance of this class to control Selenium and stores it in an easy to handle variable
+    $chromeOptions = New-Object OpenQA.Selenium.Chrome.ChromeOptions
+    $chromeOptions.AddArgument("--headless")
+    $chromeOptions.AddArgument("--disable-gpu")
+    $chromedriver = New-Object OpenQA.Selenium.Chrome.ChromeDriver($chromeOptions) # Creates an instance of this class to control Selenium and stores it in an easy to handle variable
 }
 Catch {
-    $message = ("{0}: Unexpected error creating Chrome driver. Error: {1}" -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $_.Exception.Message)
-    Write-Host $message; $message | Out-File -FilePath $logFile -Append
+    $message = ("{0}: Unexpected error starting Chrome. Error: {1}" -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $_.Exception.Message)
+    Write-Error $message; $message | Out-File -FilePath $logFile -Append
 
     Exit 1
 }
@@ -176,53 +180,119 @@ Try {
     $startTime = $chromedriver.ExecuteScript("return performance.timing.navigationStart")
     $chromedriver.Navigate().GoToURL($Url)
     Start-Sleep 5 # waiting for page load
-    $chromedriver.FindElementById("Enter user name").SendKeys($cred.Username) # Methods to find the input textbox for the username and type it into the textbox
-    $chromedriver.FindElementById("passwd").SendKeys($cred.GetNetworkCredential().password) # Methods to find the input textbox for the password and type it into the textbox
-    $chromedriver.FindElementById("passwd").Submit()
-    Start-Sleep 5
+}
+Catch {
+    $message = ("{0}: Unexpected error loading the site. {1} will exit. If available, the exception is: {1}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $MyInvocation.MyCommand, $_.Exception.Message)
+    Write-Error $message; $message | Out-File -FilePath $logFile -Append
+
+    Write-Host ("LoginSuccess=0")
+
+    Exit 1
+}
+
+If ($chromedriver.FindElementById("Enter user name")) {
+    $message = ("{0}: Attempting to log into the site." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+    Write-Host $message; $message | Out-File -FilePath $logFile -Append
+
+    Try {
+        $chromedriver.FindElementById("Enter user name").SendKeys($cred.Username) # Methods to find the input textbox for the username and type it into the textbox
+        $chromedriver.FindElementById("passwd").SendKeys($cred.GetNetworkCredential().password) # Methods to find the input textbox for the password and type it into the textbox
+        $chromedriver.FindElementById("passwd").Submit()
+    }
+    Catch {
+        $message = ("{0}: Unexpected error logging into the site. If available, the exception is: {1}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $_.Exception.Message)
+        Write-Error $message; $message | Out-File -FilePath $logFile -Append
+
+        Write-Host ("LoginSuccess=0")
+
+        Exit 1
+    }
+}
+Else {
+    $message = ("{0}: Unable to find the username field. {1} will exit" -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $MyInvocation.MyCommand)
+    Write-Error $message; $message | Out-File -FilePath $logFile -Append
+
+    Exit 1
+}
+
+$message = ("{0}: Waiting for login to complete." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+Write-Host $message; $message | Out-File -FilePath $logFile -Append
+
+Start-Sleep 5
+
+$message = ("{0}: Attempting to bypass XenApp client detection." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+Write-Host $message; $message | Out-File -FilePath $logFile -Append
+
+Try {
     $chromedriver.FindElementById("protocolhandler-welcome-installButton").Click() # "Detect client" button
-    Start-Sleep 3
+}
+Catch {
+    $message = ("{0}: Unexpected error dealing with XenApp client detection. Error: {1}" -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $_.Exception.Message)
+    Write-Error $message; $message | Out-File -FilePath $logFile -Append
+
+    $chromedriver.Close()
+    $chromedriver.Quit()
+
+    Exit 1
+}
+
+$message = ("{0}: Waiting for page load complete." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+Write-Host $message; $message | Out-File -FilePath $logFile -Append
+
+Start-Sleep 3
+
+$message = ("{0}: Attempting to access the list of published applications." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+Write-Host $message; $message | Out-File -FilePath $logFile -Append
+
+Try {
     $chromedriver.FindElementById("legalstatement-checkbox2").Click() # Accept the license to clear any hover text
     $chromedriver.FindElementById("protocolhandler-detect-alreadyInstalledLink").Click() # Skip the client install
     $endTime = $chromedriver.ExecuteScript("return window.performance.timing.domComplete")
     $duration = $endTime - $startTime
 }
 Catch {
-    $message = ("{0}: Unexpected error loading {1}. Error: {2}" -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $url, $_.Exception.Message)
+    $message = ("{0}: Unexpected error accessing the list of published applications. Error: {1}" -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $_.Exception.Message)
     Write-Error $message; $message | Out-File -FilePath $logFile -Append
 
     $chromedriver.Close()
     $chromedriver.Quit()
 
-    Write-Host ("LoginSuccess=0")
-
-    Exit 0
+    Exit 1
 }
 
-# Giving the page time to load.
+$message = ("{0}: Waiting for page load complete." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+Write-Host $message; $message | Out-File -FilePath $logFile -Append
+
 Start-Sleep 10
 
-$message = ("{0}: Looking for logoff link." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+$message = ("{0}: Attempting to logoff." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
 Write-Host $message; $message | Out-File -FilePath $logFile -Append
 
 Try {
     $chromedriver.FindElementById("userMenuBtn").Click()
     $chromedriver.FindElementById("dropdownLogOffBtn").Click()
+}
+Catch {
+    $message = ("{0}: Unexpected error logging off. Error: {1}" -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $_.Exception.Message)
+    Write-Error $message; $message | Out-File -FilePath $logFile -Append
 
     $chromedriver.Close()
     $chromedriver.Quit()
 
-    Write-Host ("LoginSuccess=1")
-    Write-Host ("LogonDurationMs={0}" -f $duration)
-
-    Exit 0
+    Exit 1
 }
-Catch {
-    $message = ("{0}: Unexpected error logging off. If available, the exception is: {1}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $_.Exception.Message)
-    Write-Error $message; $message | Out-File -FilePath $logFile -Append
 
-    Write-Host ("LoginSuccess=0")
+$message = ("{0}: Closing Chrome." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+Write-Host $message; $message | Out-File -FilePath $logFile -Append
 
-    Exit 0
-}
+$chromedriver.Close()
+$chromedriver.Quit()
+
+$message = ("{0}: Test complete, returning data to LogicMonitor." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+Write-Host $message; $message | Out-File -FilePath $logFile -Append
+
+Write-Host ("LoginSuccess=1")
+Write-Host ("LogonDurationMs={0}" -f $duration)
+
+Exit 0
 #endregion Main
