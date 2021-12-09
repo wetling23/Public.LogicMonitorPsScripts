bd40eb4b18e75d072c4c22237069afa0d17b8220
Mike Hashemi <mhashemi@synoptek.com>
2021-12-09 07:16:28 -0700

    updated logging output

--- DataSourceScripts/NimbleStorage/Get-NimbleSensorInfo/Get-NimbleSensorInfo-ActiveDiscovery.ps1
+++ DataSourceScripts/NimbleStorage/Get-NimbleSensorInfo/Get-NimbleSensorInfo-ActiveDiscovery.ps1
@@ -4,7 +4,9 @@
     .NOTES
         V1.0.0.0 date: 28 July 2020
             - Note that this DataSource requires the HPENimblePowerShellToolkit, which is available in the PowerShell Gallery.
+        V1.0.0.1 date: 9 December 2021
     .LINK
+        https://github.com/wetling23/Public.LogicMonitorPsScripts/tree/master/DataSourceScripts/NimbleStorage/Get-NimbleSensorInfo
 #>
 [CmdletBinding()]
 param ()
@@ -25,7 +27,7 @@ Else {
 $logFile = "$logDirPath\datasource-NimbleSensors-ad-$computer.log"
 
 $message = ("{0}: Beginning {1}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $MyInvocation.MyCommand)
-Write-Host $message; $message | Out-File -FilePath $logFile
+$message | Out-File -FilePath $logFile
 
 Try {
     Import-Module -Name 'HPENimblePowerShellToolkit' -ErrorAction Stop
@@ -40,13 +42,13 @@ Catch {
 
 #region Main
 $message = ("{0}: Attempting to connect to {1} as {2}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $computer, $credential.UserName)
-Write-Host $message; $message | Out-File -FilePath $logFile -Append
+$message | Out-File -FilePath $logFile -Append
 
 Try {
     Connect-NSGroup -Group $computer -Credential $credential -IgnoreServerCertificate -ErrorAction Stop
 
     $message = ("{0}: Connected to {1}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $computer)
-    Write-Host $message; $message | Out-File -FilePath $logFile -Append
+    $message | Out-File -FilePath $logFile -Append
 }
 Catch {
     $message = ("{0}: Error connecting to {1}. Error: {2}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $computer, $_.Exception.Message)
@@ -56,13 +58,13 @@ Catch {
 }
 
 $message = ("{0}: Getting shelf info." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
-Write-Host $message; $message | Out-File -FilePath $logFile -Append
+$message | Out-File -FilePath $logFile -Append
 
 $shelves = Get-NSShelf
 
 If ($shelves.ctrlrs) {
     $message = ("{0}: Returning instance data." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
-    Write-Host $message; $message | Out-File -FilePath $logFile -Append
+    $message | Out-File -FilePath $logFile -Append
 
     Foreach ($shelf in $shelves.ctrlrs) {
         Write-Host ("Side{0}-{1}##Side{0}-{1}" -f $shelf.'cid', $shelf.'ctrlr_side')
@@ -72,7 +74,7 @@ If ($shelves.ctrlrs) {
 }
 Else {
     $message = ("{0}: No shelf data returned." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
-    Write-Host $message; $message | Out-File -FilePath $logFile -Append
+    $message | Out-File -FilePath $logFile -Append
 
     Exit 1
 }
--- DataSourceScripts/NimbleStorage/Get-NimbleSensorInfo/Get-NimbleSensorInfo-Collection.ps1
+++ DataSourceScripts/NimbleStorage/Get-NimbleSensorInfo/Get-NimbleSensorInfo-Collection.ps1
@@ -5,7 +5,9 @@
         V1.0.0.0 date: 28 July 2020
             - Note that this DataSource requires the HPENimblePowerShellToolkit, which is available in the PowerShell Gallery.
             - Supports AFxx and HFxx arrays.
+        V1.0.0.1 date: 9 December 2021
     .LINK
+        https://github.com/wetling23/Public.LogicMonitorPsScripts/tree/master/DataSourceScripts/NimbleStorage/Get-NimbleSensorInfo
 #>
 [CmdletBinding()]
 param ()
@@ -26,14 +28,14 @@ Else {
 $logFile = "$logDirPath\datasource-NimbleSensors-collection-$computer.log"
 
 $message = ("{0}: Beginning {1}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $MyInvocation.MyCommand)
-Write-Host $message; $message | Out-File -FilePath $logFile
+$message | Out-File -FilePath $logFile
 
 Try {
     Import-Module -Name 'HPENimblePowerShellToolkit' -ErrorAction Stop
 }
 Catch {
     $message = ("{0}: Unexpected error importing module. Error: {1}" -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $_.Exception.Message)
-    Write-Error $message; $message | Out-File -FilePath $logFile -Append
+    $message | Out-File -FilePath $logFile -Append
 
     Exit 1
 }
@@ -41,29 +43,29 @@ Catch {
 
 #region Main
 $message = ("{0}: Attempting to connect to {1} as {2}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $computer, $credential.UserName)
-Write-Host $message; $message | Out-File -FilePath $logFile -Append
+$message | Out-File -FilePath $logFile -Append
 
 Try {
     Connect-NSGroup -Group $computer -Credential $credential -IgnoreServerCertificate -ErrorAction Stop
 
     $message = ("{0}: Connected to {1}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $computer)
-    Write-Host $message; $message | Out-File -FilePath $logFile -Append
+    $message | Out-File -FilePath $logFile -Append
 }
 Catch {
     $message = ("{0}: Error connecting to {1}. Error: {2}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $computer, $_.Exception.Message)
-    Write-Error $message; $message | Out-File -FilePath $logFile -Append
+    $message | Out-File -FilePath $logFile -Append
 
     Exit 1
 }
 
 $message = ("{0}: Getting shelf info." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
-Write-Host $message; $message | Out-File -FilePath $logFile -Append
+$message | Out-File -FilePath $logFile -Append
 
 $shelves = Get-NSShelf
 
 If (-NOT($shelves.ctrlrs)) {
     $message = ("{0}: No shelf data returned." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
-    Write-Host $message; $message | Out-File -FilePath $logFile -Append
+    $message | Out-File -FilePath $logFile -Append
 
     Exit 1
 }
