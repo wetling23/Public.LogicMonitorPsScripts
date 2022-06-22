3df98eb3793b86b4a362a33b77c4206e35aca69e
Mike Hashemi <mhashemi@synoptek.com>
2022-06-22 08:12:25 -0600

    added rate-limit handling

--- DataSourceScripts/Office365/Get-SharepointOnlineSiteStorageUtilization/SharepointOnline_Site_Storage_Utilization-ActiveDiscovery.ps1
+++ DataSourceScripts/Office365/Get-SharepointOnlineSiteStorageUtilization/SharepointOnline_Site_Storage_Utilization-ActiveDiscovery.ps1
@@ -1,74 +1,122 @@
+<#
+    .DESCRIPTION
+        Connect to Sharepoint Online, and query for a list of sites.
+    .NOTES
+        V2019.09.17.0
+        V2022.06.22.0
+    .LINK
+        https://github.com/wetling23/Public.LogicMonitorPsScripts/tree/master/DataSourceScripts/Office365/Get-SharepointOnlneSiteStorageUtilization
+#>
+[CmdletBinding()]
+param()
+
+#region Setup
 # Initialize variables.
 $adminSiteURL = '##sharepointonlineadmin.url##'
 $cred = New-Object System.Management.Automation.PSCredential ('##sharepointonlineadmin.user##', ('##sharepointonlineadmin.pass##' | ConvertTo-SecureString -AsPlainText -Force))
 
-If (Test-Path -Path "C:\Program Files (x86)\LogicMonitor\Agent\Logs" -ErrorAction SilentlyContinue) {
-    $logDirPath = "C:\Program Files (x86)\LogicMonitor\Agent\Logs" # Directory, into which the log file will be written.
-}
-Else {
+$adminSiteURL = 'https://hs2solutions-admin.sharepoint.com'
+$cred = New-Object System.Management.Automation.PSCredential ('synadmin@bounteous.com', ('Xe6eATx$Nx02#pM0' | ConvertTo-SecureString -AsPlainText -Force))
+
+If (Test-Path -Path "${env:ProgramFiles}\LogicMonitor\Agent\Logs" -ErrorAction SilentlyContinue) {
+    $logDirPath = "${env:ProgramFiles}\LogicMonitor\Agent\Logs" # Directory, into which the log file will be written.
+} ElseIf (Test-Path -Path "${env:ProgramFiles(x86)}\LogicMonitor\Agent\Logs" -ErrorAction SilentlyContinue) {
+    $logDirPath = "${env:ProgramFiles(x86)}\LogicMonitor\Agent\Logs" # Directory, into which the log file will be written.
+} Else {
     $logDirPath = "$([System.Environment]::SystemDirectory)" # Directory, into which the log file will be written.
 }
 $logFile = "$logDirPath\datasource-SharepointOnline_Site_Storage_Utilization-ad-$(($adminSiteURL.Split('/')[-1]).Split('-')[0]).log"
+#endregion Setup
 
-$message = ("{0}: Beginning {1}." -f [datetime]::Now, $MyInvocation.MyCommand)
-If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile } Else { $message | Out-File -FilePath $logFile }
+$message = ("{0}: Beginning {1}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $MyInvocation.MyCommand)
+$message | Out-File -FilePath $logFile
 
-$message = ("{0}: Checking if the Microsoft.Online.SharePoint.PowerShell module is installed." -f [datetime]::Now)
-If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+#region Install module
+$message = ("{0}: Checking if the Microsoft.Online.SharePoint.PowerShell module is installed." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+$message | Out-File -FilePath $logFile -Append
 
 If (-NOT (Get-Module -Name Microsoft.Online.SharePoint.PowerShell -ListAvailable)) {
-    $message = ("{0}: The Microsoft.Online.SharePoint.PowerShell module is not installed. Attempting to install it." -f [datetime]::Now)
-    If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+    $message = ("{0}: The Microsoft.Online.SharePoint.PowerShell module is not installed. Attempting to install it." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+    $message | Out-File -FilePath $logFile -Append
 
     Try {
         Install-Module -Name Microsoft.Online.SharePoint.PowerShell -Force
     }
     Catch {
-        $message = ("{0}: Unexpected error installing the Microsoft.Online.SharePoint.PowerShell module. The specific error is: {1}" -f [datetime]::Now, $_.Exception.Message)
-        If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+        $message = ("{0}: Unexpected error installing the Microsoft.Online.SharePoint.PowerShell module. The specific error is: {1}" -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $_.Exception.Message)
+        $message | Out-File -FilePath $logFile -Append
 
         Exit 1
     }
 }
 Else {
-    $message = ("{0}: The Microsoft.Online.SharePoint.PowerShell module is installed." -f [datetime]::Now)
-    If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+    $message = ("{0}: The Microsoft.Online.SharePoint.PowerShell module is installed." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+    $message | Out-File -FilePath $logFile -Append
 }
+#endregion Install module
 
-$message = ("{0}: Attempting to connect to {1} with {2}." -f [datetime]::Now, $adminSiteURL, $cred.UserName)
-If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+#region Auth
+$message = ("{0}: Attempting to connect to {1} with {2}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $adminSiteURL, $cred.UserName)
+$message | Out-File -FilePath $logFile -Append
 
-Try {
-    Connect-SPOService -Url $adminSiteURL –Credential $cred -ErrorAction Stop
+$stopLoop = $false
+Do {
+    Try {
+        Connect-SPOService -Url $adminSiteURL –Credential $cred -ErrorAction Stop
+
+        $stopLoop = $True
+    } Catch {
+        If ($_.Exception.Message -match '429') {
+            $message = ("{0}: Rate limit exceeded, retrying in 60 seconds." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+            $message | Out-File -FilePath $logFile -Append
+
+            Start-Sleep -Seconds 60
+        } Else {
+            $message = $message = ("{0}: Unexpected error connecting to SharePointOnline. The specific error is: {1}" -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $_.Exception.Message)
+            $message | Out-File -FilePath $logFile -Append
+
+            Exit 1
+        }
+    }
 }
-Catch {
-    $message = ("{0}: Unexpected error connecting to SharePointOnline. The specific error is: {1}" -f [datetime]::Now, $_.Exception.Message)
-    If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+While ($stopLoop -eq $false)
+#endregion Auth
 
-    Exit 1
+#region Main
+$message = ("{0}: Attempting to get the list of SharePoint sites." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+$message | Out-File -FilePath $logFile -Append
+
+$stopLoop = $false
+Do {
+    Try {
+        $siteCollections = Get-SPOSite -Limit All -ErrorAction Stop
+
+        $stopLoop = $True
+    } Catch {
+        If ($_.Exception.Message -match '429') {
+            $message = ("{0}: Rate limit exceeded, retrying in 60 seconds." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+            $message | Out-File -FilePath $logFile -Append
+
+            Start-Sleep -Seconds 60
+        } Else {
+            $message = ("{0}: Unexpected error getting SharePoint sites. The specific error is: {1}" -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $_.Exception.Message)
+            $message | Out-File -FilePath $logFile -Append
+
+            Exit 1
+        }
+    }
 }
+While ($stopLoop -eq $false)
 
-$message = ("{0}: Attempting to get the list of SharePoint sites." -f [datetime]::Now)
-If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
-
-Try {
-    $siteCollections = Get-SPOSite -Limit All -ErrorAction Stop
-}
-Catch {
-    $message = ("{0}: Unexpected error connecting getting SharePoint sites. The specific error is: {1}" -f [datetime]::Now, $_.Exception.Message)
-    If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
-
-    Exit 1
-}
-
-$message = ("{0}: Found {1} sites." -f [datetime]::Now, $siteCollections.count)
-If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+$message = ("{0}: Found {1} sites." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $siteCollections.count)
+$message | Out-File -FilePath $logFile -Append
 
 Foreach ($site in $siteCollections) {
-    $message = ("{0}: Returning {1}." -f [datetime]::Now, $site.title)
-    If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+    $message = ("{0}: Returning {1}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $site.title)
+    $message | Out-File -FilePath $logFile -Append
 
     Write-Host "$($site.url)##$($site.title)"
 }
+#endregion Main
 
 Exit 0
--- DataSourceScripts/Office365/Get-SharepointOnlineSiteStorageUtilization/SharepointOnline_Site_Storage_Utilization-Collection.ps1
+++ DataSourceScripts/Office365/Get-SharepointOnlineSiteStorageUtilization/SharepointOnline_Site_Storage_Utilization-Collection.ps1
@@ -1,67 +1,104 @@
+<#
+    .DESCRIPTION
+        Connect to Sharepoint Online, and query a site for quota data.
+    .NOTES
+        V2019.09.17.0
+        V2022.06.22.0
+    .LINK
+        https://github.com/wetling23/Public.LogicMonitorPsScripts/tree/master/DataSourceScripts/Office365/Get-SharepointOnlneSiteStorageUtilization
+#>
+[CmdletBinding()]
+param()
+
+#region Setup
 # Initialize variables.
 $siteUrl = '##wildValue##'
 $siteName = '##wildAlias##'
 $adminSiteURL = '##sharepointonlineadmin.url##'
 $cred = New-Object System.Management.Automation.PSCredential ('##sharepointonlineadmin.user##', ('##sharepointonlineadmin.pass##' | ConvertTo-SecureString -AsPlainText -Force))
 
-If (Test-Path -Path "C:\Program Files (x86)\LogicMonitor\Agent\Logs" -ErrorAction SilentlyContinue) {
-    $logDirPath = "C:\Program Files (x86)\LogicMonitor\Agent\Logs" # Directory, into which the log file will be written.
-}
-Else {
+If (Test-Path -Path "${env:ProgramFiles}\LogicMonitor\Agent\Logs" -ErrorAction SilentlyContinue) {
+    $logDirPath = "${env:ProgramFiles}\LogicMonitor\Agent\Logs" # Directory, into which the log file will be written.
+} ElseIf (Test-Path -Path "${env:ProgramFiles(x86)}\LogicMonitor\Agent\Logs" -ErrorAction SilentlyContinue) {
+    $logDirPath = "${env:ProgramFiles(x86)}\LogicMonitor\Agent\Logs" # Directory, into which the log file will be written.
+} Else {
     $logDirPath = "$([System.Environment]::SystemDirectory)" # Directory, into which the log file will be written.
 }
 $logFile = "$logDirPath\datasource-SharepointOnline_Site_Storage_Utilization-collection-$siteName.log"
+#endregion Setup
 
-$message = ("{0}: Beginning {1}." -f [datetime]::Now, $MyInvocation.MyCommand)
-If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile } Else { $message | Out-File -FilePath $logFile }
+$message = ("{0}: Beginning {1}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $MyInvocation.MyCommand)
+$message | Out-File -FilePath $logFile
 
-$message = ("{0}: Attempting to connect to {1} with {2}." -f [datetime]::Now, $adminSiteURL, $cred.UserName)
-If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+#region Auth
+$message = ("{0}: Attempting to connect to {1} with {2}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $adminSiteURL, $cred.UserName)
+$message | Out-File -FilePath $logFile -Append
 
-Try {
-    Connect-SPOService -Url $adminSiteURL –Credential $cred -ErrorAction Stop
+$stopLoop = $false
+Do {
+    Try {
+        Connect-SPOService -Url $adminSiteURL –Credential $cred -ErrorAction Stop
+
+        $stopLoop = $True
+    } Catch {
+        If ($_.Exception.Message -match '429') {
+            $message = ("{0}: Rate limit exceeded, retrying in 60 seconds." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+            $message | Out-File -FilePath $logFile -Append
+
+            Start-Sleep -Seconds 60
+        } Else {
+            $message = $message = ("{0}: Unexpected error connecting to SharePointOnline. The specific error is: {1}" -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $_.Exception.Message)
+            $message | Out-File -FilePath $logFile -Append
+
+            Exit 1
+        }
+    }
 }
-Catch {
-    $message = ("{0}: Unexpected error connecting to SharePointOnline. The specific error is: {1}" -f [datetime]::Now, $_.Exception.Message)
-    If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+While ($stopLoop -eq $false)
+#endregion Auth
 
-    Exit 1
+#region Main
+$message = ("{0}: Attempting to get the list of SharePoint sites." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+$message | Out-File -FilePath $logFile -Append
+
+$stopLoop = $false
+Do {
+    Try {
+        $site = Get-SPOSite -Identity $siteUrl -ErrorAction Stop
+
+        $stopLoop = $True
+    } Catch {
+        If ($_.Exception.Message -match '429') {
+            $message = ("{0}: Rate limit exceeded, retrying in 60 seconds." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+            $message | Out-File -FilePath $logFile -Append
+
+            Start-Sleep -Seconds 60
+        } Else {
+            $message = ("{0}: Unexpected error getting SharePoint site properties. The specific error is: {1}" -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $_.Exception.Message)
+            $message | Out-File -FilePath $logFile -Append
+
+            Exit 1
+        }
+    }
 }
-
-$message = ("{0}: Attempting to get the list of SharePoint sites." -f [datetime]::Now)
-If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
-
-Try {
-    $siteCollections = Get-SPOSite -Limit All -ErrorAction Stop
-}
-Catch {
-    $message = ("{0}: Unexpected error connecting getting SharePoint sites. The specific error is: {1}" -f [datetime]::Now, $_.Exception.Message)
-    If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
-
-    Exit 1
-}
-
-$site = $siteCollections.Where( { $_.Title -eq $siteName })
+While ($stopLoop -eq $false)
 
 If ($site.Status -eq 'Active') {
-    $data = [PSCustomObject]@{
-        Allocated   = $site.StorageQuota
-        Used        = $site.StorageUsageCurrent
-        PercentUsed = [math]::Round((($site.StorageUsageCurrent / $site.StorageQuota) * 100), 2)
-    }
+    $percentUsed = [math]::Round((($site.StorageUsageCurrent / $site.StorageQuota) * 100), 2)
 
-    $message = ("{0}: Found:`r`nAllocatedStorage={1}`r`nUsedStorage={2}`r`nPercentUsed={3}." -f [datetime]::Now, $data.Allocated, $data.Used, $data.PercentUsed)
-    If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+    $message = ("{0}: Found:`r`nAllocatedStorage={1}`r`nUsedStorage={2}`r`nPercentUsed={3}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $site.StorageQuota, $site.StorageUsageCurrent, $percentUsed)
+    $message | Out-File -FilePath $logFile -Append
 
-    Write-Host ("Allocated={0}" -f $data.Allocated)
-    Write-Host ("Used={0}" -f $data.Used)
-    Write-Host ("PercentUsed={0}" -f $data.PercentUsed)
+    Write-Host ("Allocated={0}" -f $site.StorageQuota)
+    Write-Host ("Used={0}" -f $site.StorageUsageCurrent)
+    Write-Host ("PercentUsed={0}" -f $percentUsed)
 
     Exit 0
 }
 Else {
-    $message = ("{0}: {1} is not active." -f [datetime]::Now, $siteName)
-    If (($PSBoundParameters['Verbose']) -or $VerbosePreference -eq 'Continue') { Write-Verbose $message; $message | Out-File -FilePath $logFile -Append } Else { $message | Out-File -FilePath $logFile -Append }
+    $message = ("{0}: {1} is not active." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $siteName)
+    $message | Out-File -FilePath $logFile -Append
 
     Exit 0
 }
+#endregion Main
